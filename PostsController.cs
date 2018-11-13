19c3b0e518870e900e64b4784a41e4a720150da2
vikassingh2212 <vikas.s@mind-infotech.com>
2018-11-13 20:10:53 +0530

    Update PostsController.cs

--- PostsController.cs
+++ PostsController.cs
@@ -1,24 +1,45 @@
-public HttpResponseMessage Get()
-{
-    var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/posts.json"));
-    return new HttpResponseMessage()
-    {
-        Content = new StringContent(json, Encoding.UTF8, "application/json"),
-        StatusCode = HttpStatusCode.OK
-    };
-}
+  public HttpResponseMessage Get()
+        {
+            var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/posts.json"));
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
 
-public HttpResponseMessage Get(int startIndex, int endIndex)
-{
-    var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/posts.json"));
+        public HttpResponseMessage Get(int startIndex, int endIndex)
+        {
+            var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/posts.json"));
 
 
-    JArray array = JArray.Parse(json);
-    JArray result = new JArray(array.Where(item => item.Children<JToken>().Any(p => ((JProperty)p).Name == "id" && (int)((JProperty)p).Value >= startIndex && (int)((JProperty)p).Value <= endIndex)).ToList());
+            JArray array = JArray.Parse(json);
+            JArray result = new JArray(array.Where(item => item.Children<JToken>().Any(p => ((JProperty)p).Name == "id" && (int)((JProperty)p).Value >= startIndex && (int)((JProperty)p).Value <= endIndex)).ToList());
+             
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+        public HttpResponseMessage Get(string queryString )
+        {
+            JObject queryArray = JObject.Parse(queryString);
 
-    return new HttpResponseMessage()
-    {
-        Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json"),
-        StatusCode = HttpStatusCode.OK
-    };
-}
+            int startIndex =(int)queryArray.Children<JProperty>().FirstOrDefault(t => t.Name == "first").Value;
+            int rows = (int)queryArray.Children<JProperty>().FirstOrDefault(t => t.Name == "rows").Value;
+            int endIndex = startIndex + rows;
+
+
+            var json = File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath(@"~/App_Data/posts.json"));
+
+
+            JArray array = JArray.Parse(json);
+            JArray result = new JArray(array.Where(item => item.Children<JToken>().Any(p => ((JProperty)p).Name == "id" && (int)((JProperty)p).Value >= startIndex && (int)((JProperty)p).Value <= endIndex)).ToList());
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(result.ToString(), Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
